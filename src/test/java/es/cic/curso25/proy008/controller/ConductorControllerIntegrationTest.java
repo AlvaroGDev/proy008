868ffd40cce5ba4eb92994644682dce174fd725d
AlvaroG <aayestarain@formacion.cic.es>
2025-07-21 14:00:06 +0200

    Creada estructura de test, falta añadir la funcionalidad

---
+++ src/test/java/es/cic/curso25/proy008/controller/ConductorControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package es.cic.curso25.proy008.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import es.cic.curso25.proy008.model.Conductor;
+import es.cic.curso25.proy008.repository.ConductorRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ConductorControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired 
+    ObjectMapper objectMapper;
+
+    @Autowired
+    ConductorRepository conductorRepository;
+
+    @Test
+    void testCreate() throws Exception{
+    
+        Conductor miConductor = new Conductor();
+        
+        miConductor.setNombre("Manolo");
+        miConductor.setApellido("Testeador");
+        miConductor.setTfno("123654987");
+        miConductor.setEmail("test@cic.es");
+        miConductor.setGenero(HOMBRE);
+    }
+
+
+
+
+}
