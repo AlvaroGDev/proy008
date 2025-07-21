76d9e54bc2733aeb99a87a391fda9211c10f8878
AlvaroG <aayestarain@formacion.cic.es>
2025-07-21 13:29:37 +0200

    Creados métodos CRUD de conductorService

--- src/main/java/es/cic/curso25/proy008/service/ConductorService.java
+++ src/main/java/es/cic/curso25/proy008/service/ConductorService.java
@@ -1,9 +1,34 @@
 package es.cic.curso25.proy008.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import es.cic.curso25.proy008.model.Conductor;
+import es.cic.curso25.proy008.repository.ConductorRepository;
 
 @Service
 public class ConductorService {
     
+    @Autowired
+    private ConductorRepository conductorRepository;
+
+    public Conductor create(Conductor conductor){
+
+        return conductorRepository.save(conductor);
+    }
+
+    public List<Conductor> get(){
+        return conductorRepository.findAll();
+    }
+
+    public void update(Conductor conductor){
+        conductorRepository.save(conductor);
+    }
+
+    public void delete(Long id){
+        conductorRepository.deleteById(id);
+    }
 
 }
