1578728a7fd089b5f325530991df327e5e594a22
AlvaroG <aayestarain@formacion.cic.es>
2025-07-21 13:21:43 +0200

    Creada estructura de carpetas y archivos de modelo, controlador, repositorio y tarea

---
+++ src/main/java/es/cic/curso25/proy008/controller/ConductorController.java
@@ -0,0 +1,16 @@
+package es.cic.curso25.proy008.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/conductor")
+public class ConductorController {
+    
+     private final static Logger LOGGER = LoggerFactory.getLogger(ConductorController.class);
+
+
+
+}
---
+++ src/main/java/es/cic/curso25/proy008/service/ConductorService.java
@@ -0,0 +1,9 @@
+package es.cic.curso25.proy008.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConductorService {
+    
+
+}
