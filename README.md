62a1f7bfd100cd4ae658f8c57d93c4deb6dadfa2
AlvaroG <aayestarain@formacion.cic.es>
2025-07-22 11:47:35 +0200

    Añadida modificación en README y pasado el LOGGER del controlador principal a ControllerAdvice

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 CSV
 
-control de errores (mínimo el id) en creación y modificación
-Dos entidades sin relación (Dos controladores, dos servicios, Un controller advice)
+control de errores (mínimo el id) en creación y modificación HECHO
+Dos entidades sin relación (Dos controladores, dos servicios, Un controller advice) HECHO
 los errores de la capa de servicio
 los test de la capa controlador
--- src/main/java/es/cic/curso25/proy008/controller/ConductorController.java
+++ src/main/java/es/cic/curso25/proy008/controller/ConductorController.java
@@ -17,14 +17,11 @@ import es.cic.curso25.proy008.service.ConductorService;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @RestController
 @RequestMapping("/conductor")
 public class ConductorController {
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(ConductorController.class);
+
 
     @Autowired
     private ConductorService conductorService;
