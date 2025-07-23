ed33397d54487d3392b41c6d6286b877c277201f
AlvaroG <aayestarain@formacion.cic.es>
2025-07-23 09:51:31 +0200

    Añadidos metodos andDo(print) en tests de conductor

--- src/test/java/es/cic/curso25/proy008/controller/ConductorControllerIntegrationTest.java
+++ src/test/java/es/cic/curso25/proy008/controller/ConductorControllerIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import java.util.Optional;
 
@@ -27,137 +28,144 @@ import es.cic.curso25.proy008.service.ConductorService;
 @AutoConfigureMockMvc
 public class ConductorControllerIntegrationTest {
 
-    @Autowired
-    MockMvc mockMvc;
+        @Autowired
+        MockMvc mockMvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
+        @Autowired
+        ObjectMapper objectMapper;
 
-    @Autowired
-    ConductorRepository conductorRepository;
+        @Autowired
+        ConductorRepository conductorRepository;
 
-    @Autowired
-    ConductorService conductorService;
+        @Autowired
+        ConductorService conductorService;
 
-    @Test
-    void testGet() throws Exception {
+        @Test
+        void testGet() throws Exception {
 
-        Conductor miConductor = new Conductor();
+                Conductor miConductor = new Conductor();
 
-        miConductor.setNombre("Manolo");
-        miConductor.setApellido("Testeador");
-        miConductor.setTfno("123654987");
-        miConductor.setEmail("test@cic.es");
-        miConductor.setGenero("M");
+                miConductor.setNombre("Manolo");
+                miConductor.setApellido("Testeador");
+                miConductor.setTfno("123654987");
+                miConductor.setEmail("test@cic.es");
+                miConductor.setGenero("M");
 
-        String miConductorJSON = objectMapper.writeValueAsString(miConductor);
+                String miConductorJSON = objectMapper.writeValueAsString(miConductor);
 
-        mockMvc.perform(post("/conductor")
-                .contentType("application/json")
-                .content(miConductorJSON))
-                .andExpect(status().isOk());
+                mockMvc.perform(post("/conductor")
+                                .contentType("application/json")
+                                .content(miConductorJSON))
+                                .andDo(print())
+                                .andExpect(status().isOk());
 
-        mockMvc.perform(get("/conductor/1")
-                .contentType("application/json")
-                .content(miConductorJSON))
-                .andExpect(status().isOk());
+                mockMvc.perform(get("/conductor/1")
+                                .contentType("application/json")
+                                .content(miConductorJSON))
+                                .andDo(print())
+                                .andExpect(status().isOk());
 
-    }
+        }
 
-    @Test
-    void testCreate() throws Exception {
+        @Test
+        void testCreate() throws Exception {
 
-        Conductor miConductor = new Conductor();
+                Conductor miConductor = new Conductor();
 
-        miConductor.setNombre("Manolo");
-        miConductor.setApellido("Testeador");
-        miConductor.setTfno("123654987");
-        miConductor.setEmail("test@cic.es");
-        miConductor.setGenero("M");
+                miConductor.setNombre("Manolo");
+                miConductor.setApellido("Testeador");
+                miConductor.setTfno("123654987");
+                miConductor.setEmail("test@cic.es");
+                miConductor.setGenero("M");
 
-        String miConductorJSON = objectMapper.writeValueAsString(miConductor);
+                String miConductorJSON = objectMapper.writeValueAsString(miConductor);
 
-        mockMvc.perform(post("/conductor")
-                .contentType("application/json")
-                .content(miConductorJSON))
-                .andExpect(status().isOk())
-                .andExpect(resultado -> {
-                    String respuesta = resultado.getResponse().getContentAsString();
-                    Conductor conductorPrueba = objectMapper.readValue(respuesta, Conductor.class);
-                    assertTrue(conductorPrueba.getId() > 0, "El valor debe ser mayor que 0");
+                mockMvc.perform(post("/conductor")
+                                .contentType("application/json")
+                                .content(miConductorJSON))
+                                .andDo(print())
+                                .andExpect(status().isOk())
+                                .andExpect(resultado -> {
+                                        String respuesta = resultado.getResponse().getContentAsString();
+                                        Conductor conductorPrueba = objectMapper.readValue(respuesta, Conductor.class);
+                                        assertTrue(conductorPrueba.getId() > 0, "El valor debe ser mayor que 0");
 
-                    Optional<Conductor> revisarConductor = conductorRepository.findById(conductorPrueba.getId());
-                    assertTrue(revisarConductor.isPresent());
-                });
-    }
+                                        Optional<Conductor> revisarConductor = conductorRepository
+                                                        .findById(conductorPrueba.getId());
+                                        assertTrue(revisarConductor.isPresent());
+                                });
+        }
 
-    @Test
-    void testDelete() throws Exception {
+        @Test
+        void testDelete() throws Exception {
 
-        Conductor miConductor = new Conductor();
+                Conductor miConductor = new Conductor();
 
-        miConductor.setNombre("Manolo");
-        miConductor.setApellido("el borrador");
-        miConductor.setTfno("123654987");
-        miConductor.setEmail("testBorrado@cic.es");
-        miConductor.setGenero("M");
+                miConductor.setNombre("Manolo");
+                miConductor.setApellido("el borrador");
+                miConductor.setTfno("123654987");
+                miConductor.setEmail("testBorrado@cic.es");
+                miConductor.setGenero("M");
 
-        String miConductorJSON = objectMapper.writeValueAsString(miConductor);
+                String miConductorJSON = objectMapper.writeValueAsString(miConductor);
 
-        // Creamos tarea
+                // Creamos tarea
 
-        mockMvc.perform(post("/conductor")
-                .contentType("application/json")
-                .content(miConductorJSON))
-                .andExpect(status().isOk());
+                mockMvc.perform(post("/conductor")
+                                .contentType("application/json")
+                                .content(miConductorJSON))
+                                .andDo(print())
+                                .andExpect(status().isOk());
 
-        // Revisamos que esté
+                // Revisamos que esté
 
-        mockMvc.perform(get("/conductor/1"))
-                .andExpect(status()
-                        .isOk());
+                mockMvc.perform(get("/conductor/1"))
+                                .andDo(print())
+                                .andExpect(status()
+                                                .isOk());
 
-        // La borramos
+                // La borramos
 
-        mockMvc.perform(delete("/conductor/1"))
-                .andExpect(status().isOk())
-                .andReturn();
+                mockMvc.perform(delete("/conductor/1"))
+                                .andDo(print())
+                                .andExpect(status().isOk())
+                                .andReturn();
 
-        // No haría falta revisar si no está, ya que el delete, si lo encuentra lo
-        // borra, y si no lo encuentra lo ignora
+                // No haría falta revisar si no está, ya que el delete, si lo encuentra lo
+                // borra, y si no lo encuentra lo ignora
 
-    }
+        }
 
-    @Test
-    public void testUpdate() throws Exception {
+        @Test
+        public void testUpdate() throws Exception {
 
-        Conductor miConductor = new Conductor();
+                Conductor miConductor = new Conductor();
 
-        miConductor.setNombre("Manolo");
-        miConductor.setApellido("el borrador");
-        miConductor.setTfno("123654987");
-        miConductor.setEmail("testBorrado@cic.es");
-        miConductor.setGenero("M");
+                miConductor.setNombre("Manolo");
+                miConductor.setApellido("el borrador");
+                miConductor.setTfno("123654987");
+                miConductor.setEmail("testBorrado@cic.es");
+                miConductor.setGenero("M");
 
+                // Creamos tarea
 
-        // Creamos tarea
+                conductorService.create(miConductor);
 
-        conductorService.create(miConductor);
+                // Le cambiamos el valor
 
-        // Le cambiamos el valor
+                miConductor.setNombre("NombreNuevoAntonioJose");
+                // Lo volvemos a pasar a JSON
 
-        miConductor.setNombre("NombreNuevoAntonioJose");
-        // Lo volvemos a pasar a JSON
+                String miConductorJSON = objectMapper.writeValueAsString(miConductor);
 
-       String miConductorJSON = objectMapper.writeValueAsString(miConductor);
-
-        mockMvc.perform(put("/conductor")
-                .contentType("application/json")
-                .content(miConductorJSON))
-                .andExpect(status().isOk());
+                mockMvc.perform(put("/conductor")
+                                .contentType("application/json")
+                                .content(miConductorJSON))
+                                .andDo(print())
+                                .andExpect(status().isOk());
 
                 assertEquals(miConductor.getNombre(), "NombreNuevoAntonioJose");
 
-    }
+        }
 
 }
