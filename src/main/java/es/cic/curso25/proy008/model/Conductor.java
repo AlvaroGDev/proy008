af5a7d2a598bd6372031c3a3e6c6b4f95c3d0048
AlvaroG <aayestarain@formacion.cic.es>
2025-07-29 14:00:10 +0200

    Cambios hechos dia 29 jul

--- src/main/java/es/cic/curso25/proy008/model/Conductor.java
+++ src/main/java/es/cic/curso25/proy008/model/Conductor.java
@@ -1,11 +1,17 @@
 package es.cic.curso25.proy008.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
 @Entity
@@ -22,12 +28,25 @@ public class Conductor {
     private String genero;
 
     @JsonIgnore
-    @OneToOne(mappedBy = "conductor")
+    @OneToOne(mappedBy = "conductor", cascade = { CascadeType.PERSIST, CascadeType.REMOVE, CascadeType.MERGE })
     private Viaje viaje;
-    // El ignore básicamente le dice que a la hora de crear un viaje junto con el conductor, no lo haga
-    // en principio, el orden debería ser, un conductor se puede crear por si solo, pero un viaje DEBE llamar a un conductor y crearlo si no existe uno
+    /*
+     * El ignore básicamente le dice que a la hora de crear un viaje junto con el
+     * conductor, no lo haga
+     * en principio, el orden debería ser, un conductor se puede crear por si solo,
+     * pero un viaje DEBE llamar a un conductor y crearlo si no existe uno
+     * Es importante tener cuidado con el modo cascada en el remove. Si lo tenemos
+     * en viaje (Que depende de conductor) al borrar un viaje
+     * borrariamos el conductor también. Igual lo queremos o igual no, eso depende
+     * de lo que queramos hacer
+     */
 
-    public Conductor(){
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    // El mapped by hace dos cosas, la primera es decirnos quien va a tener la foreign key 
+    // Cuando creo la mascota, creo el conductor
+    private List<Mascota> mascotas = new ArrayList<>(); 
+
+    public Conductor() {
 
     }
 
@@ -41,54 +60,60 @@ public class Conductor {
         this.viaje = viaje;
     }
 
-    
     public Long getId() {
         return id;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getApellido() {
         return apellido;
     }
+
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+
     public String getTfno() {
         return tfno;
     }
+
     public void setTfno(String tfno) {
         this.tfno = tfno;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     };
 
-     public void setGenero(String genero) {
+    public void setGenero(String genero) {
         this.genero = genero;
     }
 
-    public String getGenero(){
+    public String getGenero() {
         return genero;
     }
 
-    public Viaje getViaje(){
+    public Viaje getViaje() {
         return viaje;
     }
 
-    public void setViaje(Viaje viaje){
+    public void setViaje(Viaje viaje) {
         this.viaje = viaje;
     }
 
-    
 }
